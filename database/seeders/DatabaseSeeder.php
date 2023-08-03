75945332a7ee8843afdf33ef88a953392e55ac15
gudangcoding <dientrimedia@gmail.com>
2023-08-03 08:15:34 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        //seed users table
+    \App\Models\User::factory(2)->create();
+    //seed articles table
+    \App\Models\Article::factory(50)->create();
+    }
+}
